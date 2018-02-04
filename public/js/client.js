a07fa25fdad3a334ee54c6ebf33fd17de2b7b33b
kevinmartinez <kevin@pixelkojan.se>
2018-02-04 14:01:40 +0100

    handle user input and send to server and markup

--- public/js/client.js
+++ public/js/client.js
@@ -4,32 +4,39 @@ const html =
   <heading>
     <h2 class="moo">Hello</h2>
     <div class="output"></div>
-    <form id="form">
-      <input type="text">
-      <button type="submit">
-    </form>
   </heading>
 </section>
 `
 
-document.getElementById('main').innerHTML = html
-
-const form = document.getElementById('form')
-const output = document.querySelector('.output')
+document.getElementById('botAnswer').innerHTML = html
 
 // Create a new WebSocket instance
 const ws = new WebSocket('ws://localhost:8080')
 
-ws.onopen = () => {
-  console.log('socket io client hello')
-}
+// WebSocket on connection
+ws.onopen = () => console.log('socket connected')
 
-ws.onclose = () => {
-  console.log('user disconnnected')
-}
+// WebSocket on close
+ws.onclose = () => console.log('socket disconnnected')
 
-ws.onmessage = (message) => {
-  output.innerHTML = message.data
-}
+// WebSocket emit message
+ws.onmessage = (message) => printMessage(message.data)
 
-console.log(form)
+// Select our form that will contain the messages
+const chatForm = document.querySelector('.chat-form')
+
+chatForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  let input = document.getElementById('message')
+  // Send input to server
+  ws.send(input.value)
+  // Clean up input field
+  input.value = ''
+})
+
+// Print user message to document
+const printMessage = (message) => {
+  const p = document.createElement('p')
+  p.innerText = message
+  document.querySelector('div.messages').appendChild(p)
+}
