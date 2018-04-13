63cf46f84ff7918012b0ae3bfd4005572f73fb06
kevinmartinez <kevin@pixelkojan.se>
2018-04-13 19:22:28 +0200

    WIP add proper questions

--- public/js/client.js
+++ public/js/client.js
@@ -1,41 +1,10 @@
 'use strict'
 
-var host = location.origin.replace(/^http/, 'ws')
-
+const host = location.origin.replace(/^http/, 'ws')
 const ws = new WebSocket(host)
 
-// const ws = new WebSocket('ws://www.host.com/path', {
-//   perMessageDeflate: false
-// });
-
-// ws.onerror = (error) => {
-//   console.log(error)
-// }
-
-// let userId
-// // Get a unique id stamp
-// userId = new Date()
-// console.log(`userId: ${userId}`)
-
-// WebSocket on connection, first 'style'
-ws.addEventListener('open', () => {
-  ws.send(`haiii [from: CLIENT]`)
-})
-
-// WebSocket on connection, second 'style'
-ws.onopen = (message, error) => {
-  // console.log('from client.js: ', message)
-  // console.log(message.data)
-}
-
 // WebSocket emit message
-ws.onmessage = (message, id) => {
-  printMessage(message.data)
-  console.log(id)
-}
-
-// WebSocket on close
-ws.onclose = () => console.log('FRONT-END', 'socket disconnnected')
+ws.onmessage = (message) => printMessage(message.data)
 
 // Select our form that will contain the messages
 const chatForm = document.querySelector('.chat-form')
@@ -55,39 +24,38 @@ chatForm.addEventListener('submit', (event) => {
 // To create a proper 'auto-scrolling' when new messages
 // are recieved
 const scrollDown = (element, parent) => {
-  // console.log('scrollDown()')
   const topPos = element.offsetTop
-  // console.log(topPos)
   parent.scrollTop = topPos
 }
 
-// Create a 'create element' function
-// TODO: advance upon it
+// Why? Create elements easier
 const newElement = (element) => {
   return document.createElement(element)
 }
 
+// Why? Log messages
 const timeStamp = () => {
   const time = new Date()
   return time.toLocaleTimeString('en-GB')
 }
 
-// TODO: redo front-end logic, with a templating language
-// PUG it
-// Print user/cpu message to document
+// Why? Output messages
 const printMessage = (message) => {
   const time = timeStamp()
   const li = newElement('li')
   const p = newElement('p')
-  if (message.includes('iFriend') || message === 'Welcome') {
+
+  if (message.includes('iFriend') || message.includes('disconnected')) {
     li.classList = 'chat__box chat__box--cpu'
     p.classList = 'chat__text chat__text--cpu'
   } else {
     li.classList = 'chat__box chat__box--user'
     p.classList = 'chat__text chat__text--user'
   }
+
   li.innerHTML = `<header>${time}</header>`
   p.innerText = `${message}`
+
   document.querySelector('ul.messages').appendChild(li).appendChild(p)
   scrollDown(li, phoneCase)
 }
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 'use strict'
-// Create an express app by requiring the express module
+
 const express = require('express')
 const http = require('http')
 const WebSocket = require('ws')
 const app = express() // Get an application instance of 'express'
 const port = process.env.PORT || 5000
-
-// Assign chatbots answers
 const api = require('./api/answers.json')
 
 let questions = api.map((item) => {
@@ -52,20 +50,13 @@ wss.on('connection', (ws, req) => {
     console.log(error.stack)
     console.log(error.message)
   })
-  // const location = url.parse(req.url, true)
-  // console.log(location)
+
   // Add listeners to the WebSocket
   ws.on('message', (message, id) => {
     id = userID
-    // msg = message
-    // console.log(`message from: ${message} [ ...SERVER]`)
-    console.log(`msg from id: ${id}`)
-    console.log(message)
-    console.log('id log', id)
 
     if (message === 'exit') {
-      console.log(this)
-      ws.send(`User #${id} quit`)
+      ws.send(`User #${id}, you have disconnected`)
       ws.close()
     } else {
       // All socket clients are placed in an array
@@ -85,32 +76,23 @@ wss.on('connection', (ws, req) => {
           }
         })
 
-        if (message === questions[0]) {
-          client.send(`iFriend: ${api[0].answer}`)
+        // Loop through Q/A API
+        for (let i = 0; i < questions.length; i++) {
+          console.log(questions[i])
+          if (message.toLowerCase() === questions[i]) {
+            client.send(`iFriend: ${api[i].answer}`)
+          }
         }
       })
     }
-    ws.on('close', () => {
+    ws.on('close', (message) => {
       console.log(`User ${id} is 'CLOSING DOWN`)
     })
   })
+
   // Static welcome message sent from server
-  ws.send(`Welcome`)
+  ws.send(`iFriend: Hello friend, Do you have any questions? If you want tips about questions to ask, type '-help'`)
 })
 
 // The express app should listen to this port
 server.listen(port, () => console.log(`Listening on port: ${server.address().port}`))
-
-// By exporting this app instance as a module,
-// it can be included in other files, e.g test files
-module.exports = app
-
-// Middleware to log each request, when we have a request,
-// the express app will use this middleware,
-// BEFORE going to the static. This adds functionality to our pipeline,
-// it will go through the pipeline, through the use methods when we have,
-// a request, until it get a response
-// app.use((request, response, next) => {
-//   console.log(`${request.method} request for ${request.url}`)
-//   next()
-// })
